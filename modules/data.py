69d03465713fba889692f0a9267d745d7fbd8fb1
daniloldn <112765545+daniloldn@users.noreply.github.com>
2025-11-03 17:26:35 +0000

    exercise 1 PS2

--- modules/data.py
+++ modules/data.py
@@ -1,41 +1,37 @@
 
 import pandas as pd
 
-def read_data(path):
+
+def read_data(path: str) -> pd.DataFrame:
 
     df = pd.read_csv(path)
     return df
 
-def select_columns(df):
+
+def select_columns(df: pd.DataFrame) -> pd.DataFrame:
     df = df[["Reference area", "TIME_PERIOD", "OBS_VALUE"]]
-    df.rename(columns={"Reference area" : "Country",
-                    "TIME_PERIOD" : "Year",
-                    "OBS_VALUE" : "GDP (millions USD)"}, inplace=True)
-    
+    df.rename(columns={"Reference area": "Country",
+                       "TIME_PERIOD": "Year",
+                       "OBS_VALUE": "GDP (millions USD)"}, inplace=True)
+
     df["Year"] = pd.to_datetime(df["Year"])
 
     return df
 
 
-
-def missing_data(df):
-
+def missing_data(df: pd.DataFrame) -> pd.DataFrame:
     num_missing = df.isnull().sum()
-
     if num_missing > 0:
         df_clean = df.dropna()
         return df_clean
     else:
         return df
-    
 
-def load_data(path):
+
+def load_data(path: str) -> pd.DataFrame:
 
     df = read_data(path)
-    df =select_columns(df)
+    df = select_columns(df)
     df = missing_data(df)
 
     return df
-
-
-
--- modules/plot_utils.py
+++ modules/plot_utils.py
@@ -1,14 +1,16 @@
 # %%
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
-def make_charts(dataframe):
+
+def make_charts(dataframe: pd.DataFrame) -> None:
     fig, ax = plt.subplots(figsize=(10, 6))
-    sns.lineplot(data= dataframe, x='Year', y='GDP (millions USD)', hue='Country', ax = ax)
+    sns.lineplot(data=dataframe, x='Year', y='GDP (millions USD)',
+                 hue='Country', ax=ax)
     ax.set_xlabel('Year')
     ax.set_ylabel('GDP (million USD)')
     ax.set_title('GDP series')
     ax.legend
     plt.tight_layout()
     plt.show()
-
